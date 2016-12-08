574fe96f9873876bc14fb871f9678448bfaa8ff7
Mallikarjuna <drop2ms@gmail.com>
2016-12-08 15:19:37 +0530

    Added JPA Annotations to Entity Files

---
+++ src/main/java/com/teamtreehouse/project/model/Project.java
@@ -0,0 +1,52 @@
+package com.teamtreehouse.project.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ * Created by Mallikarjuna on 12/8/2016.
+ */
+@Entity
+public class Project {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  enum Status {Archived,Active,NotStarted}
+  private Status status;
+  @ManyToMany
+  private List<Role> rolesNeeded = new ArrayList<>();
+  @ManyToMany
+  private List<Collaborator> collaborators = new ArrayList<>();
+
+  public Long getId() {return id;}
+
+  public void setId(Long id) {this.id = id;}
+
+  public String getName() {return name;}
+
+  public void setName(String name) {this.name = name;}
+
+  public String getDescription() {return description;}
+
+  public void setDescription(String description) {this.description = description;}
+
+  public List<Role> getRolesNeeded() {return rolesNeeded;}
+
+  public void setRolesNeeded(List<Role> rolesNeeded) {this.rolesNeeded = rolesNeeded;}
+
+  public List<Collaborator> getCollaborators() {return collaborators;}
+
+  public void setCollaborators(List<Collaborator> collaborators) {
+    this.collaborators = collaborators;
+  }
+
+  public Status getStatus() {return status;}
+}
---
+++ src/main/java/com/teamtreehouse/project/model/Role.java
@@ -0,0 +1,27 @@
+package com.teamtreehouse.project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Mallikarjuna on 12/8/2016.
+ */
+@Entity
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  public Role() {}
+
+  public Long getId() {return id;}
+
+  public void setId(Long id) {this.id = id;}
+
+  public String getName() {return name;}
+
+  public void setName(String name) {this.name = name;}
+}
