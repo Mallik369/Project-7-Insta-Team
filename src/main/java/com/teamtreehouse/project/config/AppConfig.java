c165efbfe9b9b389a7c6bd8d7bcf46c358b79fe0
Mallikarjuna <drop2ms@gmail.com>
2016-12-08 10:55:42 +0530

    Modfied Import Package of Environment

--- src/main/java/com/teamtreehouse/project/config/AppConfig.java
+++ src/main/java/com/teamtreehouse/project/config/AppConfig.java
@@ -1,7 +1,5 @@
 package com.teamtreehouse.project.config;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
