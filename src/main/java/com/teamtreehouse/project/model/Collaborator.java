e5e7750eb7163dc0dd8801f5d0b5e83d2e9fbd9b
Mallikarjuna <drop2ms@gmail.com>
2016-12-30 11:38:37 +0530

    Added Parameterized  Constructor to Collaborator Entity

--- src/main/java/com/teamtreehouse/project/model/Collaborator.java
+++ src/main/java/com/teamtreehouse/project/model/Collaborator.java
@@ -20,6 +20,10 @@ public class Collaborator {
 
   public Collaborator() {}
 
+  public Collaborator(String name) {
+    this.name = name;
+  }
+
   public Long getId() {return id;}
 
   public void setId(Long id) {this.id = id;}
