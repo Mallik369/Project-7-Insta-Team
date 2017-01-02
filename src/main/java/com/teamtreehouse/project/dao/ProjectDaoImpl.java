94f3345a913367266b39100699b2edef7b5a526f
Mallikarjuna <drop2ms@gmail.com>
2017-01-02 20:09:00 +0530

    Add Project Dao to List Project and Intializate Collaborators and Roles of Project in Session

--- src/main/java/com/teamtreehouse/project/dao/ProjectDaoImpl.java
+++ src/main/java/com/teamtreehouse/project/dao/ProjectDaoImpl.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.project.dao;
 
 import com.teamtreehouse.project.model.Project;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,11 @@ public class ProjectDaoImpl implements ProjectDao {
   @Override
   @SuppressWarnings("unchecked")
   public List<Project> listAllProjects() {
-    return null;
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<Project> projectList = session.createCriteria(Project.class).list();
+    session.close();
+    return projectList;
   }
 
   @Override
@@ -29,6 +34,8 @@ public class ProjectDaoImpl implements ProjectDao {
     Session session = sessionFactory.openSession();
     session.beginTransaction();
     Project project = session.get(Project.class,id);
+    Hibernate.initialize(project.getCollaborators());
+    Hibernate.initialize(project.getRolesNeeded());
     session.close();
     return project;
   }
